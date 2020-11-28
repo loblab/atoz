b0a0c3548b3b3b1b1076bc4357837fd5f962a7fd
loblab <11693722+loblab@users.noreply.github.com>
2020-11-28 12:10:48 +0800

    Save hit speed to database

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,6 @@ ARG PYTHON=python3
 RUN apt-get update --fix-missing && apt-get -y upgrade
 RUN apt-get -y install ${PYTHON}-pip
 RUN $PYTHON -m pip install flask
+RUN $PYTHON -m pip install influxdb
+RUN apt-get -y install influxdb-client
 
--- service.py
+++ service.py
@@ -25,14 +25,35 @@ def echo():
     app.logger.info(req)
     return jsonify(req)
 
-@app.route('/score', methods=['POST'])
+@app.route('/api/score', methods=['POST'])
 def api_score():
     req = request.json
     ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
-    save_score(req["keys"], req["score"], ip)
-    return jsonify(req)
+    resp = {}
+    try:
+        save_score(req["keys"], req["score"], ip)
+        resp["status"] = "OK"
+    except Exception as e:
+        msg = str(e)
+        app.logger.error(msg)
+        resp["status"] = "ERR"
+        resp["msg"] = msg
+    return jsonify(resp)
+
+from influxdb import InfluxDBClient
 
 def save_score(keys, score, ip):
+    host = "influxdb"
+    port = 8086
+    user = ""
+    pswd = ""
+    data = "atoz"
+    db = InfluxDBClient(host, port, user, pswd, data, timeout=5)
+
+    now = time.time()
+    ts = int(now * 1e9)
+    points = []
+
     app.logger.info(ip)
     app.logger.info(keys)
     app.logger.info(score)
@@ -42,5 +63,33 @@ def save_score(keys, score, ip):
         val = score[i]
         total += val
         app.logger.info("%s: %d ms", key, val)
+        point = {
+            "measurement": "speed",
+            "time": ts,
+            "tags": {
+                "ip": ip,
+                "key": key
+            },
+            "fields": {
+                "val": val
+            }
+        }
+        points.append(point)
+
     app.logger.info("%s: %d ms", keys, total)
+    point = {
+        "measurement": "speed",
+        "time": ts,
+        "tags": {
+            "ip": ip,
+            "key": keys
+        },
+        "fields": {
+            "val": total
+        }
+    }
+    points.append(point)
+    app.logger.info(points)
+    db.write_points(points)
+    db.close()
 
